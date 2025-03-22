73dfeee990a2d06425972627a5829cbb7d3c9c24
InFinity_54 <corentinkreicher@gmail.com>
2025-03-22 03:07:56 +0100

    Premiers logos TV + apparence de la grille d'affichage des logos

---
+++ js/tvdb.js
@@ -0,0 +1,28 @@
+function getDatabase() {
+    return [
+        {
+            "name": "Radiovision-PTT",
+            "id": "radiovision_ptt_01",
+            "start": "1935-04",
+            "end": "1937-06"
+        },
+        {
+            "name": "Radiodiffusion nationale Télévision",
+            "id": "rtt_01",
+            "start": "1937-07",
+            "end": "1939-07"
+        },
+        {
+            "name": "Radiodiffusion nationale Télévision",
+            "id": "rtt_02",
+            "start": "1939-08",
+            "end": "1939-09"
+        },
+        {
+            "name": "Radiodiffusion nationale Télévision",
+            "id": "rtt_01",
+            "start": "1939-09",
+            "end": "1940-05"
+        }
+    ];
+}
