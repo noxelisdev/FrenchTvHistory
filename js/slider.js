ed3b66606e6cfacaed25c8c15e784f900b134cb7
InFinity_54 <corentinkreicher@gmail.com>
2025-03-22 18:57:19 +0100

    Affichage des chaînes TV en fonction du mois sélectionné

--- js/slider.js
+++ js/slider.js
@@ -8,18 +8,39 @@ const totalMonths = (endYear - startYear) * 12 + (endMonth - startMonth) + 1;
 const monthSlider = document.getElementById("month");
 const selectedMonthText = document.getElementById("currentMonth");
 
-function getMonthYear(index) {
-    let year = startYear + Math.floor((startMonth + index) / 12);
-    let month = (startMonth + index) % 12;
+function getMonthYear(year, month) {
     const monthNames = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
     return `${monthNames[month]} ${year}`;
 }
 
-function updateLabel() {
-    selectedMonthText.textContent = getMonthYear(parseInt(monthSlider.value));
+function updateLabel(year, month) {
+    selectedMonthText.textContent = getMonthYear(year, month);
+}
+
+function updateList(year, month) {
+    const selectedMonth = String(year) + "-" + String(month).padStart(2, "0");
+    console.log(year, month);
+
+    Array.prototype.forEach.call(document.getElementsByClassName("tv"), function(tv) {
+        tv.style.display = "none";
+    });
+
+    Array.prototype.forEach.call(getDatabase(selectedMonth), function(tvId) {
+        document.getElementById(tvId).style.display = "block";
+        console.log(tvId);
+    });
+}
+
+function updateDisplay() {
+    const currentValue = parseInt(monthSlider.value);
+    let year = startYear + Math.floor((startMonth + currentValue) / 12);
+    let month = (startMonth + currentValue) % 12;
+
+    updateLabel(year, month);
+    updateList(year, month + 1);
 }
 
 monthSlider.max = totalMonths - 1;
-monthSlider.addEventListener("input", updateLabel);
+monthSlider.addEventListener("input", updateDisplay);
 
-updateLabel();
+updateDisplay();
--- js/tvdb.js
+++ js/tvdb.js
@@ -1,28 +1,99 @@
-function getDatabase() {
-    return [
-        {
-            "name": "Radiovision-PTT",
-            "id": "radiovision_ptt_01",
-            "start": "1935-04",
-            "end": "1937-06"
-        },
-        {
-            "name": "Radiodiffusion nationale Télévision",
-            "id": "rtt_01",
-            "start": "1937-07",
-            "end": "1939-07"
-        },
-        {
-            "name": "Radiodiffusion nationale Télévision",
-            "id": "rtt_02",
-            "start": "1939-08",
-            "end": "1939-09"
-        },
-        {
-            "name": "Radiodiffusion nationale Télévision",
-            "id": "rtt_01",
-            "start": "1939-09",
-            "end": "1940-05"
-        }
-    ];
+function getDatabase(month) {
+    switch (month) {
+        default:
+            return [];
+        case "1935-04":
+        case "1935-05":
+        case "1935-06":
+        case "1935-07":
+        case "1935-08":
+        case "1935-09":
+        case "1935-10":
+        case "1935-11":
+        case "1935-12":
+        case "1936-01":
+        case "1936-02":
+        case "1936-03":
+        case "1936-04":
+        case "1936-05":
+        case "1936-06":
+            return [
+                "radiovision_ptt_01"
+            ];
+        case "1936-07":
+        case "1936-08":
+        case "1936-09":
+        case "1936-10":
+        case "1936-11":
+        case "1936-12":
+        case "1937-01":
+        case "1937-02":
+        case "1937-03":
+        case "1937-04":
+        case "1937-05":
+        case "1937-06":
+        case "1937-07":
+        case "1937-08":
+        case "1937-09":
+        case "1937-10":
+        case "1937-11":
+        case "1937-12":
+        case "1938-01":
+        case "1938-02":
+        case "1938-03":
+        case "1938-04":
+        case "1938-05":
+        case "1938-06":
+        case "1938-07":
+        case "1938-08":
+        case "1938-09":
+        case "1938-10":
+        case "1938-11":
+        case "1938-12":
+        case "1939-01":
+        case "1939-02":
+        case "1939-03":
+        case "1939-04":
+        case "1939-05":
+        case "1939-06":
+        case "1939-07":
+            return [
+                "rtt_01"
+            ];
+        case "1939-08":
+        case "1939-09":
+            return [
+                "rtt_02"
+            ];
+        case "1939-10":
+        case "1939-11":
+        case "1939-12":
+        case "1940-01":
+        case "1940-02":
+        case "1940-03":
+        case "1940-04":
+        case "1940-05":
+            return [
+                "rtt_01"
+            ];
+        case "1943-05":
+        case "1943-06":
+        case "1943-07":
+        case "1943-08":
+        case "1943-09":
+        case "1943-10":
+        case "1943-11":
+        case "1943-12":
+        case "1944-01":
+        case "1944-02":
+        case "1944-03":
+        case "1944-04":
+        case "1944-05":
+        case "1944-06":
+        case "1944-07":
+        case "1944-08":
+            return [
+                "fernsehsender_paris_01"
+            ];
+    }
 }
